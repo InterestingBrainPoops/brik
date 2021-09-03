faf9f9b875caaeb51f478354d8d4b88337408e27
InterestingBrainPoops <satasiyahet@gmail.com>
2021-09-02 19:34:04 -0700

    Added other fns that ill need

--- src/state.rs
+++ src/state.rs
@@ -47,12 +47,23 @@ impl Turn {
 }
 
 impl State {
+    /// Make a legal move on the board.
     pub fn make_move(&mut self, move_to_make: Move) {
         self.turn.flip();
         self.pieces[move_to_make.location.y][move_to_make.location.x] = move_to_make.piece.into();
     }
+    /// Undo a given move on the board.
     pub fn undo_move(&mut self, move_to_undo: Move) {
         self.turn.flip();
         self.pieces[move_to_undo.location.y][move_to_undo.location.x] = move_to_undo.piece.into();
+    }
+    /// Checks if the game is over, if it is, then return the side who won. Otherwise return None.
+    pub fn is_game_end(&self) -> Option<Turn> {
+        todo!()
+    }
+    /// Returns moves for the current side to play.
+    // TODO: Switch this to ArrayVec if that speeds this up.
+    pub fn get_moves(&self) -> Vec<Move> {
+        todo!()
     }
 }
